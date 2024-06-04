16c5fa5f5134546e23725c22f747133ee169f9be
Chris Elliott <acelliott1218@gmail.com>
2024-06-04 17:01:59 +0000

    add google sheets functionality, add variables to main() for proper interactions

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
-#need to connect to spreadsheet
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Food Survey')
 
 
 def inputer(prompt):
@@ -16,13 +28,15 @@ def talker(words):
 
 def survey(diet, name):
     """
-    Separates vegetarians from non-vegetarians for data clarity
+    The main survey function, which will collect the data inputted by survey takers
+    Meat questions are reserved for non-vegetarians as to not skew the data
     """
     talker("Let's get started! What do you think of...")
     question1 = inputer("Apples, Bananas, and Mangoes? Please separate your answers by commas, for example \n 5,3,10\n")
+    #code to raise exception if the value isn't an integer
     talker("Very good! Moving onto vegetables, what do you think of...")
     question2 = inputer("Cucumbers, tomatoes, and potatoes?\n Tomatoes aren't technically fruits, but you know what we mean! Please separate your answers by commas, for example \n 5,3,10\n ")
-    #code to add +1 to survey respondents
+    #code to raise exception if the value isn't an integer
     answer1 = question1.split(",")
     answer2 = question2.split(",")
     if diet == "no" or "n":
@@ -33,8 +47,10 @@ def survey(diet, name):
         #code to raise exception if the value isn't an integer
         print(answer3)
         return answer3
+        #code to add numeric value to the spreadsheet
     print(answer1,answer2)
     return answer1, answer2
+    #code to add numeric value to the spreadsheet
 
 # function to add survey results to relevant spreadsheet
 
@@ -44,10 +60,14 @@ def survey(diet, name):
 
 def main():
     info = talker("Welcome to the Food Survey! You will be asked your opinions on various food groups.\n")
-    name = inputer("What is your name?\n").title()
-    diet = inputer("Are you vegetarian?\n").lower() #this needs to be strictly yes/no, will figure that out
+    name = inputer("What is your name?\n").title() #code to restrict this to strings at certain character limits
+    diet = inputer("Are you vegetarian? Please enter 'yes' or 'no'.\n").lower() #this needs to be strictly yes/no, will figure that out
     review = talker(f'Welcome {name}! Since you answered "{diet}", your survey will be tailored with this in mind!')
     survey(diet, name)
+    non_vegetarian = SHEET.worksheet('Standard')
+    vegetarian = SHEET.worksheet('Vegetarian')
+    standard_data = non_vegetarian.get_all_values()
+    veg_data = vegetarian.get_all_values()
 
 if __name__ == "__main__":
     main()
