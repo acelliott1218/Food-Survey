e30901087db04cf60a873688ae910106f8d9cb9f
Chris Elliott <acelliott1218@gmail.com>
2024-06-05 15:56:56 +0000

    create Big class, put survey and appender within said class. Appender not yet functional.

--- run.py
+++ run.py
@@ -26,59 +26,87 @@ def talker(words):
     """ 
     print(words)
 
-def survey(diet, name):
+class Big():
     """
-    The main survey function, which will collect the data inputted by survey takers
-    Meat questions are reserved for non-vegetarians as to not skew the data
+    This gargantuan class combines the survive logic AND the appending logic
+    while allowing easy access to the answer attributes across the class its self
     """
-    try:
-        talker("On a scale of 1-10, rate: Apples, Bananas, and Mangoes")
-        
-        question1 = inputer("Please separate your answers by commas, for example \n 5,3,10\n")
-        
-        cleaner1 = question1.split(",")
-        
-        answer1 = [int(i) for i in cleaner1]
-        # refresher from senderle https://stackoverflow.com/questions/6009589/how-to-test-if-every-item-in-a-list-of-type-int
-        
-        if len(answer1) > 3:
-            raise ValueError("You put in too many numbers!")
-    except ValueError as e:
-        print(f"Error! {e} try again, just numbers this time, and 3 of 'em!")
-    
-    try:
-        talker("Very good! Moving onto vegetables...")
-        
-        talker("On a scale of 1-10, rate: Cucumbers, tomatoes, and potatoes.")
-        
-        talker("Tomatoes aren't technically fruits, but you know what we mean!")
-        
-        question2 = inputer("Please separate your answers by commas, for example:\n5,3,10\n")
-        
-        cleaner2 = question2.split(",")
-        
-        answer2 = [int(x) for x in cleaner2]
-        
-        if len(answer2) > 3:
-            raise ValueError("You put in too many numbers!")
-                # refresher from senderle https://stackoverflow.com/questions/6009589/how-to-test-if-every-item-in-a-list-of-type-int 
-    except ValueError as e:
-            rint(f"Error! {e} try again, and just numbers this time!")
-    
-    try: 
-        if diet == "no" or diet == "n":
-            talker(f"Very good! So, {name}, let's talk meat: what do you think about...")
-            question3 = inputer("Beef, chicken, and pork? Please separate your answers by commas, for example \n 5,3,10\n")
-            cleaner3 = question3.split(",")
-            answer3 = [int(x) for x in cleaner3]
-            if len(answer2) > 3:
+    def __init__(self):
+        self.answer1 = None
+        self.answer2 = None
+        self.answer3 = None
+    def survey(self, diet, name):
+        """
+        The main survey function, which will collect the data inputted by survey takers
+        Meat questions are reserved for non-vegetarians as to not skew the data
+        """
+        try:
+            talker("On a scale of 1-10, rate: Apples, Bananas, and Mangoes")
+            
+            question1 = inputer("Please separate your answers by commas, for example \n 5,3,10\n")
+            #cleaner1, cleaner2, and cleaner3 all serve to prepare the inputted data for int-conversion
+            cleaner1 = question1.split(",")
+            #iterates through the list provided from question1, without commas, and converts to integers for the final answer
+            self.answer1 = [int(i) for i in cleaner1]
+            # refresher from senderle https://stackoverflow.com/questions/6009589/how-to-test-if-every-item-in-a-list-of-type-int
+            
+            if len(self.answer1) > 3:
                 raise ValueError("You put in too many numbers!")
-        elif diet == "yes" or diet == "y":
-            return answer1, answer2
-    except ValueError as e:
-        print(f"Error! {e} try again, just numbers this time, and 3 of 'em!")
-    else:
-        return answer1,answer2,answer3
+        except ValueError as e:
+            print(f"Error! {e} try again, just numbers this time, and 3 of 'em!")
+        
+        try:
+            #this entire section works the exact same as the previous one
+            talker("Very good! Moving onto vegetables...")
+            
+            talker("On a scale of 1-10, rate: Cucumbers, tomatoes, and potatoes.")
+            
+            talker("Tomatoes aren't technically fruits, but you know what we mean!")
+            
+            question2 = inputer("Please separate your answers by commas, for example:\n5,3,10\n")
+            
+            cleaner2 = question2.split(",")
+            
+            self.answer2 = [int(x) for x in cleaner2]
+            
+            if len(self.answer2) > 3:
+                raise ValueError("You put in too many numbers!")
+                    # refresher from senderle https://stackoverflow.com/questions/6009589/how-to-test-if-every-item-in-a-list-of-type-int 
+        except ValueError as e:
+                rint(f"Error! {e} try again, and just numbers this time!")
+        
+        try: 
+            #section to separate data from non-vegetarians
+            if diet == "no" or diet == "n":
+
+                talker(f"Very good! So, {name}, let's talk meat: what do you think about...")
+                
+                question3 = inputer("Beef, chicken, and pork? Please separate your answers by commas, for example \n 5,3,10\n")
+                
+                cleaner3 = question3.split(",")
+                
+                self.answer3 = [int(x) for x in cleaner3]
+                
+                if len(self.answer2) > 3:
+                    raise ValueError("You put in too many numbers!")
+            
+            elif diet == "yes" or diet == "y":
+                return self.answer1, self.answer2
+        # this exception is unfortunately not where you'd think it'd be!
+        except ValueError as e:
+            print(f"Error! {e} try again, just numbers this time, and 3 of 'em!")
+        if diet == "no" or diet == "n":
+            return self.answer1,self.answer2,self.answer3
+
+    def appender(self, veg_data, standard_data, diet):
+        if diet == "yes" or diet == "y":
+            veg_data.append(self.answer1)
+            veg_data.append(self.answer2)
+        else:
+            standard_data.append(self.answer1)
+            standard_data.append(self.answer2)
+            standard_data.append(self.answer3)
+
 
 
 
@@ -93,13 +121,15 @@ def survey(diet, name):
 def main():
     info = talker("Welcome to the Food Survey! You will be asked your opinions on various food groups.\n")
     name = inputer("What is your name?\n").title() #code to restrict this to strings at certain character limits
-    diet = inputer("Are you vegetarian? Please enter 'yes' or 'no'.\n").lower() #this needs to be strictly yes/no, will figure that out
+    diet = inputer("Are you vegetarian? Please enter 'yes' or 'no'.\n").lower()
     review = talker(f'Welcome {name}! Since you answered "{diet}", your survey will be tailored with this in mind!')
-    survey(diet, name)
     non_vegetarian = SHEET.worksheet('Standard')
     vegetarian = SHEET.worksheet('Vegetarian')
     standard_data = non_vegetarian.get_all_values()
     veg_data = vegetarian.get_all_values()
+    b = Big()
+    b.survey(diet, name)
+    b.appender(veg_data, standard_data,diet)
 
 if __name__ == "__main__":
     main()
